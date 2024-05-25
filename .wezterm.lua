2fe22862dee15712f07feedf3c7f88987d5ae5e5
OwLu0905 <owlu0905@gmail.com>
2024-05-26 00:12:17 +0800

    add wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,32 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Catppuccin Frappe'
+
+config.font = wezterm.font 'FiraCode Nerd Font Mono'
+config.font_size = 14
+
+config.line_height = 1.2
+
+config.window_decorations = 'RESIZE'
+
+config.hide_tab_bar_if_only_one_tab = true
+
+config.window_padding = {
+	left = 20,
+	right = 20,
+	top = 32,
+	bottom = 0,
+}
+
+config.term = "wezterm"
+-- config.term = 'wezterm'
+
+-- and finally, return the configuration to wezterm
+return config
