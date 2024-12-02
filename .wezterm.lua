a69cea3ead4d67b14bccb8fac5ac44ee9f5c0939
OwLu0905 <owlu0905@gmail.com>
2024-12-02 22:59:27 +0800

    chore: lua format

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -7,14 +7,14 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Catppuccin Frappe'
+config.color_scheme = "Catppuccin Frappe"
 
-config.font = wezterm.font 'FiraCode Nerd Font Mono'
+config.font = wezterm.font("FiraCode Nerd Font Mono")
 config.font_size = 14
 
 config.line_height = 1.2
 
-config.window_decorations = 'RESIZE'
+config.window_decorations = "RESIZE"
 
 config.hide_tab_bar_if_only_one_tab = true
 
@@ -26,7 +26,10 @@ config.window_padding = {
 }
 
 config.term = "wezterm"
--- config.term = 'wezterm'
+
+config.keys = {
+	{ key = " ", mods = "SHIFT|CTRL", action = wezterm.action.QuickSelect },
+}
 
 -- and finally, return the configuration to wezterm
 return config
